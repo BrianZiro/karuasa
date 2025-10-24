a0f5666c30ff0871b3334689df9c099acfd29858
BrianZiro <brianziro44@gmail.com>
2025-10-24 16:00:45 +0300

    Render

--- karuasa/settings.py
+++ karuasa/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@)-5u--wvn5)+^bg+^f8b5^%ewz45(c3jzjnpzkyw83sw*66e4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['karuasa.onrender.com']
 
 
 # Application definition
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
